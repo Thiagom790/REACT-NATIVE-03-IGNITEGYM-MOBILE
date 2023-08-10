29c5d7223f1ecbdaab0eb23d95932c786502c148
Thiago Marques Texeira <thiagom790@gmail.com>
2023-08-10 08:13:07 -0300

    feat: add signin screen

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
 import { NativeBaseProvider } from "native-base";
 import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }); // carregando as fontes
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Fontes carregadas</Text> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
