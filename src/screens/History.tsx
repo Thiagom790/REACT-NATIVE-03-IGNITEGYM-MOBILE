91af60639229b8f7e58af13ab1b4808a8285da1d
Thiago Marques Texeira <thiagom790@gmail.com>
2023-09-05 07:42:01 -0300

    refactor: fetch history from backend

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,22 +1,48 @@
-import { useState } from "react";
-import { Heading, SectionList, VStack, Text } from "native-base";
+import { useCallback, useState } from "react";
+import { Heading, SectionList, VStack, Text, useToast } from "native-base";
+import { AppError } from "@utils/AppError";
+import { useFocusEffect } from "@react-navigation/native";
+import { api } from "@services/api";
 
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryByDayDTO } from "@dtos/HistoryByDayDTO";
 
 export function History() {
-  const [exercises, setExercises] = useState([
-    { title: "26.8.22", data: ["Costas", "Puxada frontal"] },
-    { title: "27.8.22", data: ["Remada", "Puxada supinada"] },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const toast = useToast();
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([]);
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/history");
+      setExercises(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar o histórico.";
+
+      toast.show({ title, placement: "top", bgColor: "red.500" });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory();
+    }, [])
+  );
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
       <SectionList
         sections={exercises}
-        keyExtractor={(item) => item}
-        renderItem={() => <HistoryCard />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <HistoryCard data={item} />}
         renderSectionHeader={({ section }) => (
           <Heading
             color="gray.200"
